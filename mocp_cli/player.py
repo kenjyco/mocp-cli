e597b050a8f04d6b79b0d9073dc4e36f30627bfe
Ken <kenjyco@gmail.com>
2017-10-08 07:42:12 -0500

    Update player.py to use OrderedDict for chfunc (and re-order)

--- mocp_cli/player.py
+++ mocp_cli/player.py
@@ -1,28 +1,30 @@
 import moc
 import input_helper as ih
 from functools import partial
+from collections import OrderedDict
 from chloop import GetCharLoop
 
 
-chfunc = {
-    'H': (partial(moc.seek, -30), 'rewind 30 seconds'),
-    'h': (partial(moc.seek, -5), 'rewind 5 seconds'),
-    '\x1b[D': (partial(moc.seek, -1), '(left arrow) rewind 1 second'),
-    'L': (partial(moc.seek, 30), 'fast foward 30 seconds'),
-    'l': (partial(moc.seek, 5), 'fast foward 5 seconds'),
-    '\x1b[C': (partial(moc.seek, 1), '(right arrow) fast foward 1 second'),
-    ' ': (moc.toggle_pause, 'pause/unpause'),
-    'i': (lambda: print(moc.info_string()), 'show info about currently playing file'),
-    'n': (moc.next, 'next file in playlist'),
-    'p': (moc.previous, 'previous file in playlist'),
-    'j': (moc.volume_down, 'lower volume'),
-    '\x1b[B': (moc.volume_down, '(down arrow) lower volume'),
-    'k': (moc.volume_up, 'raise volume'),
-    '\x1b[A': (moc.volume_up, '(up arrow) raise volume'),
-}
+chfunc = OrderedDict([
+    (' ', (moc.toggle_pause, 'pause/unpause')),
+    ('i', (lambda: print(moc.info_string()), 'show info about currently playing file')),
+    ('n', (moc.next, 'next file in playlist')),
+    ('p', (moc.previous, 'previous file in playlist')),
+    ('H', (partial(moc.seek, -30), 'rewind 30 seconds')),
+    ('h', (partial(moc.seek, -5), 'rewind 5 seconds')),
+    ('\x1b[D', (partial(moc.seek, -1), 'rewind 1 second (left arrow)')),
+    ('L', (partial(moc.seek, 30), 'fast foward 30 seconds')),
+    ('l', (partial(moc.seek, 5), 'fast foward 5 seconds')),
+    ('\x1b[C', (partial(moc.seek, 1), 'fast foward 1 second (right arrow)')),
+    ('j', (moc.volume_down, 'lower volume')),
+    ('\x1b[B', (moc.volume_down, 'lower volume (down arrow)')),
+    ('k', (moc.volume_up, 'raise volume')),
+    ('\x1b[A', (moc.volume_up, 'raise volume (up arrow)')),
+])
 
 
 class _Player(GetCharLoop):
+    """A wrapper to control moc (music on console) player with vim keybindings"""
     def seek(self, num):
         """Seek forward or backward"""
         moc.seek(int(num))
