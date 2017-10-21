e6edd9a70831787b5e9f92eed70015f403d5716c
Ken <kenjyco@gmail.com>
2017-10-21 11:05:21 -0700

    Add logger to __init__.py

--- mocp_cli/__init__.py
+++ mocp_cli/__init__.py
@@ -1,1 +1,19 @@
+import logging
+import os.path
+
+
+LOGFILE = os.path.abspath('log--mocp-cli.log')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(LOGFILE, mode='a')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(logging.Formatter(
+    '%(asctime)s - %(levelname)s - %(funcName)s: %(message)s'
+))
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(logging.Formatter('%(asctime)s: %(message)s'))
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
 from .player import Player
