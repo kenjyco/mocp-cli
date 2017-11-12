e482c1e72d8bcbdb893619e455667fb74a6d2571
Ken <kenjyco@gmail.com>
2017-11-12 08:44:14 -0600

    Update help strings in 'chfunc' for non-printing chars to not say the char name

--- mocp_cli/player.py
+++ mocp_cli/player.py
@@ -243,14 +243,14 @@ chfunc = OrderedDict([
     ('p', (moc.previous, 'previous file in playlist')),
     ('H', (partial(moc.seek, -30), 'rewind 30 seconds')),
     ('h', (partial(moc.seek, -5), 'rewind 5 seconds')),
-    ('\x1b[D', (partial(moc.seek, -1), 'rewind 1 second (left arrow)')),
+    ('\x1b[D', (partial(moc.seek, -1), 'rewind 1 second')),
     ('L', (partial(moc.seek, 30), 'fast foward 30 seconds')),
     ('l', (partial(moc.seek, 5), 'fast foward 5 seconds')),
-    ('\x1b[C', (partial(moc.seek, 1), 'fast foward 1 second (right arrow)')),
+    ('\x1b[C', (partial(moc.seek, 1), 'fast foward 1 second')),
     ('j', (moc.volume_down, 'lower volume')),
-    ('\x1b[B', (moc.volume_down, 'lower volume (down arrow)')),
+    ('\x1b[B', (moc.volume_down, 'lower volume')),
     ('k', (moc.volume_up, 'raise volume')),
-    ('\x1b[A', (moc.volume_up, 'raise volume (up arrow)')),
+    ('\x1b[A', (moc.volume_up, 'raise volume')),
 ])
 
 
