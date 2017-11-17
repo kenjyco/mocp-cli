670a97400b3573f998c94ea7d5492aeeaf5951ca
Ken <kenjyco@gmail.com>
2017-11-17 00:12:21 -0600

    Add 'get_current_basename' and define many funcs based on import status of yt_helper

--- mocp_cli/player.py
+++ mocp_cli/player.py
@@ -12,6 +12,7 @@ try:
 
 except ImportError:
     input_hook = None
+    pre_input_hook = None
 
     def mark_it():
         logger.debug(moc.info_string('{currentsec} seconds into {file}'))
@@ -19,21 +20,57 @@ except ImportError:
     def get_comments(**kwargs):
         pass
 
-    def show_comments():
+    def show_comments(**kwargs):
         #
         # Maybe try to grep for `moc.get_info_dict().get('file')` in logger file
         #
         pass
 
-    def select_comments():
+    def select_comments(**kwargs):
+        pass
+
+    def jump_to_select():
+        pass
+
+    def jumploop(**kwargs):
+        pass
+
+    def get_paths_from_basenames(*args):
+        pass
+
+    def play_basenames(*args):
+        pass
+
+    def delete(**kwargs):
+        pass
+
+    def recent_files_play_select(**kwargs):
+        pass
+
+    def most_commented_files_play_select(**kwargs):
+        pass
+
+    def edit_comment_timestamp_select():
+        pass
+
+    def delete_comments_select():
         pass
 else:
+    def get_current_basename():
+        """Return basename of current playing file"""
+        return get_real_basename(moc.get_info_dict().get('file'))
+
     def input_hook(**kwargs):
-        basename = get_real_basename(moc.get_info_dict().get('file'))
+        basename = get_current_basename()
         COMMENTS.add(
             basename=basename,
             **kwargs
         )
+
+    def pre_input_hook():
+        return {
+            'timestamp': moc.get_info_dict().get('currentsec')
+        }
 
     def mark_it():
         input_hook(text='mark', **pre_input_hook())
@@ -49,7 +86,7 @@ else:
             kwargs['admin_fmt'] = True
         if 'item_format' not in kwargs:
             kwargs['all_fields'] = True
-        basename = get_real_basename(moc.get_info_dict().get('file'))
+        basename = get_current_basename()
         return COMMENTS.find(
             'basename:{}'.format(basename),
             **kwargs
@@ -101,179 +138,167 @@ else:
             unbuffered=unbuffered
         )
 
+    def jump_to_select():
+        """Use select_comments to select a mark/comment for the current file
 
-def pre_input_hook():
-    return {
-        'timestamp': moc.get_info_dict().get('currentsec')
-    }
+        Jump immediately to timestamp and prompt for a note if the mark has no note
+        """
+        selected = select_comments()
+        if selected:
+            moc.go(selected[0]['timestamp'])
+            if selected[0]['text'] in ('mark', ''):
+                comment = ih.user_input_fancy('note for mark')
+                if comment != {'text': ''}:
+                    COMMENTS.update(
+                        selected[0]['_id'],
+                        **comment
+                    )
 
+    def jumploop(choose_all=False):
+        """Loop an unbuffered input session, jumping between selected marks (up to 52)"""
+        num_comments = len(get_comments(get_fields='timestamp', include_meta=False))
+        if choose_all:
+            selected = get_comments(
+                post_fetch_sort_key='timestamp',
+                sort_key_default_val=0
+            )
+        else:
+            selected = select_comments(
+                prompt='Select up to 52 comments for jumploop (or type "all")',
+                unbuffered=False,
+                get_fields='text,timestamp',
+                include_meta=False
+            )
+        if selected:
+            basename = get_current_basename()
+            selected = selected[:52]
+            while True:
+                print('\n{}\n'.format(basename))
+                try:
+                    idx = ih.make_selections(
+                        selected,
+                        item_format='{timestamp} -> {text}',
+                        prompt='Select mark/comment or ctrl+c to break loop',
+                        wrap=False,
+                        unbuffered=True,
+                        raise_interrupt=True,
+                        raise_exception_chars=[' ']
+                    )
+                    if idx:
+                        if basename not in moc.info_string():
+                            play_basenames(basename)
+                            moc.go(idx[0]['timestamp'])
+                        else:
+                            moc.go(idx[0]['timestamp'])
+                except KeyboardInterrupt:
+                    print()
+                    break
+                except Exception:
+                    moc.toggle_pause()
 
-def jump_to_select():
-    """Use select_comments to select a mark/comment for the current file
+    def get_paths_from_basenames(*basenames):
+        """Return a list of path globs for basenames (determined from FILES)"""
+        return [
+            '{}/{}*'.format(FILES[basename].get('dirname', ''), basename)
+            for basename in basenames
+        ]
 
-    Jump immediately to timestamp and prompt for a note if the mark has no note
-    """
-    selected = select_comments()
-    if selected:
-        moc.go(selected[0]['timestamp'])
-        if selected[0]['text'] in ('mark', ''):
-            comment = ih.user_input_fancy('note for mark')
-            if comment != {'text': ''}:
-                COMMENTS.update(
-                    selected[0]['_id'],
-                    **comment
-                )
+    def play_basenames(*basenames):
+        """Use FILES to generate path globs to basenames and play"""
+        paths = get_paths_from_basenames(*basenames)
+        if paths:
+            moc.find_and_play(*paths)
 
+    def delete(basename=None):
+        """Delete audio file and remove related data from COMMENTS
 
-def jumploop(choose_all=False):
-    """Loop an unbuffered input session, jumping between selected marks (up to 52)"""
-    if choose_all:
-        selected = get_comments(
-            post_fetch_sort_key='timestamp',
-            sort_key_default_val=0
+        In FILES, the item will be updated to 'audio=False' in case there is a
+        related video file of the same name (which this will not delete)
+        """
+        if basename is None:
+            basename = get_current_basename()
+            if not basename:
+                return
+        paths = get_paths_from_basenames(basename)
+        if paths:
+            files = moc.find_audio(*paths)
+            if files:
+                file_id = FILES[basename].get('_id')
+                comment_ids = [
+                    x['_id']
+                    for x in COMMENTS.find('basename:{}'.format(basename), get_fields='_id')
+                ]
+                print('For {}, found {} file(s) and {} comment(s)'.format(
+                    repr(basename),
+                    len(files),
+                    len(comment_ids)
+                ))
+                yn = ih.user_input('\nAre you sure you want to delete? (y/n)')
+                if yn.lower().startswith('y'):
+                    for f in files:
+                        try:
+                            remove(f.strip('\'"'))
+                        except Exception as e:
+                            logger.error('Could not delete {}... {}'.format(repr(f), repr(e)))
+                        else:
+                            logger.info('Deleted {}'.format(repr(f)))
+                    if file_id:
+                        FILES.update(file_id, audio=False)
+                    if comment_ids:
+                        COMMENTS.delete_many(*comment_ids)
+
+    def recent_files_play_select(limit=25):
+        """Select files that were most recently added and play"""
+        selected = ih.make_selections(
+            FILES.find('audio:True', get_fields='basename', admin_fmt=True, limit=limit),
+            item_format='{basename} .::. {_ts}',
+            prompt='Select basenames to play',
+            wrap=False
         )
-    else:
-        selected = select_comments(
-            prompt='Select up to 52 comments for jumploop (or type "all")',
-            unbuffered=False
+        if selected:
+            play_basenames(*[x['basename'] for x in selected])
+
+    def most_commented_files_play_select(limit=25):
+        """Select files that have been most commented and play"""
+        selected = ih.make_selections(
+            COMMENTS.top_values_for_index('basename', limit=limit),
+            prompt='Select basenames to play',
+            item_format='{} ({})',
+            wrap=False
         )
-    if selected:
-        basename = get_real_basename(moc.get_info_dict().get('file'))
-        selected = selected[:52]
-        while True:
-            print('\n{}\n'.format(basename))
-            try:
-                idx = ih.make_selections(
-                    selected,
-                    item_format='{timestamp} -> {text}',
-                    prompt='Select mark/comment or ctrl+c to break loop',
-                    wrap=False,
-                    unbuffered=True,
-                    raise_interrupt=True,
-                    raise_exception_chars=[' ']
-                )
-                if idx:
-                    if idx[0]['basename'] not in moc.info_string():
-                        play_basenames(idx[0]['basename'])
-                        moc.go(idx[0]['timestamp'])
-                    else:
-                        moc.go(idx[0]['timestamp'])
-            except KeyboardInterrupt:
-                print()
-                break
-            except Exception:
-                moc.toggle_pause()
+        if selected:
+            play_basenames(*[s[0] for s in selected])
 
+    def edit_comment_timestamp_select():
+        """Use select_comments to select a mark/comment for the current file
 
-def get_paths_from_basenames(*basenames):
-    """Return a list of path globs for basenames (determined from FILES)"""
-    return [
-        '{}/{}*'.format(FILES[basename].get('dirname', ''), basename)
-        for basename in basenames
-    ]
+        Prompt for a new timestamp (usually +/-1 from current timestamp)
+        """
+        selected = select_comments(prompt='Select mark/comment to edit timestamp')
+        if selected:
+            prompt = 'Enter new timestamp (old was {})'.format(selected[0].get('timestamp', ''))
+            timestamp = ih.user_input(prompt)
+            if timestamp:
+                try:
+                    timestamp = int(timestamp)
+                except ValueError:
+                    print('{} is not an integer'.format(timestamp))
+                else:
+                    _id = selected[0]['_id']
+                    COMMENTS.update(_id, timestamp=timestamp)
+        else:
+            print()
 
-
-def play_basenames(*basenames):
-    """Use FILES to generate path globs to basenames and play"""
-    paths = get_paths_from_basenames(*basenames)
-    if paths:
-        moc.find_and_play(*paths)
-
-
-def delete(basename=None):
-    """Delete audio file and remove related data from COMMENTS
-
-    In FILES, the item will be updated to 'audio=False' in case there is a
-    related video file of the same name (which this will not delete)
-    """
-    if basename is None:
-        basename = get_real_basename(moc.get_info_dict().get('file'))
-        if not basename:
-            return
-    paths = get_paths_from_basenames(basename)
-    if paths:
-        files = moc.find_audio(*paths)
-        if files:
-            file_id = FILES[basename].get('_id')
-            comment_ids = [
-                x['_id']
-                for x in COMMENTS.find('basename:{}'.format(basename), get_fields='_id')
-            ]
-            print('For {}, found {} file(s) and {} comment(s)'.format(
-                repr(basename),
-                len(files),
-                len(comment_ids)
-            ))
-            yn = ih.user_input('\nAre you sure you want to delete? (y/n)')
-            if yn.lower().startswith('y'):
-                for f in files:
-                    try:
-                        remove(f.strip('\'"'))
-                    except Exception as e:
-                        logger.error('Could not delete {}... {}'.format(repr(f), repr(e)))
-                    else:
-                        logger.info('Deleted {}'.format(repr(f)))
-                if file_id:
-                    FILES.update(file_id, audio=False)
-                if comment_ids:
-                    COMMENTS.delete_many(*comment_ids)
-
-
-def recent_files_play_select(limit=25):
-    """Select files that were most recently added and play"""
-    selected = ih.make_selections(
-        FILES.find('audio:True', get_fields='basename', admin_fmt=True, limit=limit),
-        item_format='{basename} .::. {_ts}',
-        prompt='Select basenames to play',
-        wrap=False
-    )
-    if selected:
-        play_basenames(*[x['basename'] for x in selected])
-
-
-def most_commented_files_play_select(limit=25):
-    """Select files that have been most commented and play"""
-    selected = ih.make_selections(
-        COMMENTS.top_values_for_index('basename', limit=limit),
-        prompt='Select basenames to play',
-        item_format='{} ({})',
-        wrap=False
-    )
-    if selected:
-        play_basenames(*[s[0] for s in selected])
-
-
-def edit_comment_timestamp_select():
-    """Use select_comments to select a mark/comment for the current file
-
-    Prompt for a new timestamp (usually +/-1 from current timestamp)
-    """
-    selected = select_comments(prompt='Select mark/comment to edit timestamp')
-    if selected:
-        prompt = 'Enter new timestamp (old was {})'.format(selected[0].get('timestamp', ''))
-        timestamp = ih.user_input(prompt)
-        if timestamp:
-            try:
-                timestamp = int(timestamp)
-            except ValueError:
-                print('{} is not an integer'.format(timestamp))
-            else:
-                _id = selected[0]['_id']
-                COMMENTS.update(_id, timestamp=timestamp)
-    else:
-        print()
-
-
-def delete_comments_select():
-    """Use COMMENTS.select_and_modify to choose which comments to delete
-    """
-    basename = get_real_basename(moc.get_info_dict().get('file'))
-    return COMMENTS.select_and_modify(
-        action='delete',
-        terms='basename:{}'.format(basename),
-        prompt='Select comments to delete (separate by space)',
-        menu_item_format='{timestamp} -> {text} .::. {_ts}'
-    )
+    def delete_comments_select():
+        """Use COMMENTS.select_and_modify to choose which comments to delete
+        """
+        basename = get_current_basename()
+        return COMMENTS.select_and_modify(
+            action='delete',
+            terms='basename:{}'.format(basename),
+            prompt='Select comments to delete (separate by space)',
+            menu_item_format='{timestamp} -> {text} .::. {_ts}'
+        )
 
 
 chfunc = OrderedDict([
