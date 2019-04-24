08005dc95d4242e671698ddea97697059256c123
Ken <kenjyco@gmail.com>
2019-04-24 05:32:50 -0500

    Update dev-setup.bash to use eager upgrade strategy

--- dev-setup.bash
+++ dev-setup.bash
@@ -4,4 +4,10 @@
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
 [[ ! -d "venv" ]] && python3 -m venv venv && venv/bin/pip3 install --upgrade pip wheel
-venv/bin/pip3 install -r requirements.txt --upgrade
+pip_args=(--upgrade)
+pip_version=$(venv/bin/pip3 --version | egrep -o 'pip (\d+)' | cut -c 5-)
+[[ -z "$pip_version" ]] && pip_version=$(venv/bin/pip3 --version | perl -pe 's/^pip\s+(\d+).*/$1/')
+[[ -z "$pip_version" ]] && pip_version=0
+[[ $pip_version -gt 9 ]] && pip_args=(--upgrade --upgrade-strategy eager)
+venv/bin/pip3 install -r requirements.txt ${pip_args[@]}
+venv/bin/python3 setup.py develop
