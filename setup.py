aefc47b886e1571c1a3f2bf3f0a2ade63422f4b8
Ken <kenjyco@gmail.com>
2021-03-03 06:16:05 -0600

    Bump to v0.1.18

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open('README.rst', 'r') as fp:
 
 setup(
     name='mocp-cli',
-    version='0.1.17',
+    version='0.1.18',
     description='CLI tools for finding, organizing, and playing audio files',
     long_description=long_description,
     author='Ken',
     author_email='kenjyco@gmail.com',
     license='MIT',
     url='https://github.com/kenjyco/mocp-cli',
-    download_url='https://github.com/kenjyco/mocp-cli/tarball/v0.1.17',
+    download_url='https://github.com/kenjyco/mocp-cli/tarball/v0.1.18',
     packages=find_packages(),
     install_requires=[
         'bg-helper',
